a11b63b5035618a5b937eabc094026bfc61d0598
Helen Fornazier <helen@maedro.com>
2015-06-04 00:53:20 -0300

    lib: mv: example: Fix in live mode
    The live packets should go to the same interface that requested it.

--- sketchbook/libraries/Movuino/Examples/MvFrameHandler_AccGyro_Test/MvFrameHandler_AccGyro_Test.ino
+++ sketchbook/libraries/Movuino/Examples/MvFrameHandler_AccGyro_Test/MvFrameHandler_AccGyro_Test.ino
@@ -62,10 +62,13 @@ struct app_context
 {
     struct live_ctl
     {
-        int en;
         unsigned long period;
         unsigned long time_stamp;
+        MvCom *com_list[MAX_COM_PORTS];
+        /* ref counts how many com ports are in the live mode com list*/
+        int ref;
     } live_ctl;
+
     MvCom *com;
     MvFrameHandler *fhandler;
     MvAccGyro accgyro;
@@ -74,9 +77,14 @@ struct app_context
 
 void setup()
 {
-    g_ctx.live_ctl.en = 0;
+    int i;
+
+    g_ctx.live_ctl.ref = 0;
     g_ctx.live_ctl.time_stamp = 0;
     g_ctx.live_ctl.period = 500000; // in micro seconds
+
+    for (i = 0; i < MAX_COM_PORTS; i++)
+        g_ctx.live_ctl.com_list[i] == NULL;
 
     Serial.begin(38400);
 
@@ -121,6 +129,58 @@ void set_live_sampling_rate(unsigned int hz)
     g_ctx.live_ctl.period = 1000000/hz;
 }
 
+void add_com_to_live_list(MvCom *com)
+{
+    int i;
+
+    /* Check if it is already on the list */
+    for (i = 0; i < MAX_COM_PORTS; i++)
+    {
+        if (g_ctx.live_ctl.com_list[i] == com)
+            return;
+    }
+
+    /* Find a null entry */
+    for (i = 0; i < MAX_COM_PORTS; i++)
+    {
+        if (g_ctx.live_ctl.com_list[i] == NULL)
+        {
+            g_ctx.live_ctl.com_list[i] = com;
+            g_ctx.live_ctl.ref++;
+            return;
+        }
+    }
+}
+
+void remove_com_from_live_list(MvCom *com)
+{
+    int i;
+
+    for (i = 0; i < MAX_COM_PORTS; i++)
+    {
+        if (g_ctx.live_ctl.com_list[i] == com)
+        {
+            g_ctx.live_ctl.com_list[i] = NULL;
+            g_ctx.live_ctl.ref--;
+            return;
+        }
+    }
+}
+
+void send_live(struct frame *frame)
+{
+    int i;
+
+    for (i = 0; i < MAX_COM_PORTS; i++)
+    {
+        if (g_ctx.live_ctl.com_list[i] != NULL)
+        {
+            frame->com = g_ctx.live_ctl.com_list[i];
+            g_ctx.fhandler->write_frame(frame);
+        }
+    }
+}
+
 void loop()
 {
     int ans_err = ANS_NACK_UNKNOWN_CMD;
@@ -131,8 +191,6 @@ void loop()
         switch(g_ctx.frame.cmd.id)
         {
             case CMD_PING:
-                Serial.print("Mv Live en:");
-                Serial.println(g_ctx.live_ctl.en);
                 Serial.print("Mv Live period:");
                 Serial.println(g_ctx.live_ctl.period);
                 Serial.print("Mv Live ts:");
@@ -144,15 +202,28 @@ void loop()
                 break;
 
             case CMD_LIVE_START:
-                ans_err = g_ctx.accgyro.open();
-                if (!ans_err) g_ctx.live_ctl.en = true;
+                ans_err = 0;
+
+                /* Check if the acc has already been initialized */
+                if (!g_ctx.live_ctl.ref)
+                    ans_err = g_ctx.accgyro.open();
+
+                if (!ans_err)
+                    add_com_to_live_list(g_ctx.frame.com);
+
                 send_ack_nack(&g_ctx.frame, g_ctx.fhandler, ans_err);
                 break;
 
             case CMD_LIVE_STOP:
-                ans_err = g_ctx.accgyro.close();
-                if (!ans_err) g_ctx.live_ctl.en = false;
-                send_ack_nack(&g_ctx.frame, g_ctx.fhandler, ans_err);
+                ans_err = 0;
+
+                remove_com_from_live_list(g_ctx.frame.com);
+
+                /* If there is no more com ports in live mode */
+                if (!g_ctx.live_ctl.ref)
+                    ans_err = g_ctx.accgyro.close();
+
+                send_ack_nack(&g_ctx.frame, g_ctx.fhandler, 0);
                 break;
 
             case CMD_CONFIG_SET:
@@ -217,7 +288,7 @@ void loop()
     }
 
     // Deal with live
-    if (g_ctx.live_ctl.en)
+    if (g_ctx.live_ctl.ref)
     {
         // Check if its time to print the next data
         if(check_live_time(g_ctx.live_ctl.time_stamp, g_ctx.live_ctl.period))
@@ -241,27 +312,27 @@ void loop()
             // Send raw acc data
             g_ctx.frame.answer.sub.sensor_data.type = SENS_ACC_RAW;
             g_ctx.frame.answer.sub.sensor_data.data.raw = g_ctx.accgyro.get_raw_acc();
-            g_ctx.fhandler->write_frame(&g_ctx.frame);
+            send_live(&g_ctx.frame);
 
             // Send raw gyro data
             g_ctx.frame.answer.sub.sensor_data.type = SENS_GYRO_RAW;
             g_ctx.frame.answer.sub.sensor_data.data.raw = g_ctx.accgyro.get_raw_gyro();
-            g_ctx.fhandler->write_frame(&g_ctx.frame);
+            send_live(&g_ctx.frame);
 
             // Send quat data
             g_ctx.frame.answer.sub.sensor_data.type = SENS_QUAT;
             g_ctx.frame.answer.sub.sensor_data.data.quat = g_ctx.accgyro.get_quat();
-            g_ctx.fhandler->write_frame(&g_ctx.frame);
+            send_live(&g_ctx.frame);
 
             // Send euler data
             g_ctx.frame.answer.sub.sensor_data.type = SENS_EULER;
             g_ctx.frame.answer.sub.sensor_data.data.euler = g_ctx.accgyro.get_euler();
-            g_ctx.fhandler->write_frame(&g_ctx.frame);
+            send_live(&g_ctx.frame);
 
             // Send gravity data
             g_ctx.frame.answer.sub.sensor_data.type = SENS_GRAVITY;
             g_ctx.frame.answer.sub.sensor_data.data.gravity = g_ctx.accgyro.get_gravity();
-            g_ctx.fhandler->write_frame(&g_ctx.frame);
+            send_live(&g_ctx.frame);
 
             //Serial.print("Mv etime:");
             //Serial.println(micros() - g_ctx.live_ctl.time_stamp);
