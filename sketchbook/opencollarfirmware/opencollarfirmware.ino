0c8cb5f1b453f341d51c72dc40e6781f6a866a91
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2015-07-10 16:54:58 +0200

    Add a header to help Arduino find the altimeter library

--- sketchbook/opencollarfirmware/opencollarfirmware.ino
+++ sketchbook/opencollarfirmware/opencollarfirmware.ino
@@ -1,3 +1,4 @@
+
 /* ============================================
 I2Cdev device library code is placed under the MIT license
 Copyright (c) 2011 Jeff Rowberg
@@ -34,6 +35,7 @@ THE SOFTWARE.
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
+#include <MS5611.h>
 //#include "MPU6050.h"
 
 #include "MPU6050_6Axis_MotionApps20.h"
